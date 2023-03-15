9616bfccbb12ec4768a52ea91011ad255547c14a
jonathanpsantos <jonathan.santos194@etec.sp.gov.br>
2023-03-14 22:13:33 -0300

    0.11

--- app/src/main/java/br/com/etecia/recyclerviewfilmes/MainActivity.java
+++ app/src/main/java/br/com/etecia/recyclerviewfilmes/MainActivity.java
@@ -4,12 +4,31 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+
+    List<Filmes> listaFilmes;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //criando a base de dados para carregamento da lista de filmes
+        listaFilmes = new ArrayList<>();
+
+        listaFilmes.add(
+
+                new Filmes("Creed: nascido para lutar",
+                        "Adonis Johnson never knew his famous father, world heavyweight champion Apollo Creed, who died before he was born. Still, there's no denying that boxing is in his blood, so Adonis heads to Philadelphia, the site of Apollo Creed's legendary match with a tough upstart named Rocky Balboa. ",
+                        "Ação",R.drawable.creed)
+        );
+
+
+        //criando a classe adaptadore passando os parêmetros
+        MyAdapter adapter = new MyAdapter(getApplicationContext(),listaFilmes);
     }
 }
--- app/src/main/java/br/com/etecia/recyclerviewfilmes/MyAdapter.java
+++ app/src/main/java/br/com/etecia/recyclerviewfilmes/MyAdapter.java
@@ -1,12 +1,32 @@
 package br.com.etecia.recyclerviewfilmes;
 
+import android.content.Context;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.List;
+
 public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder>{
+
+    private Context mContexto;
+    private List<Filmes> listFilmes;
+
+    //criando o construdor da classe com parâmetros
+
+    public MyAdapter(Context mContexto, List<Filmes> listFilmes) {
+        this.mContexto = mContexto;
+        this.listFilmes = listFilmes;
+    }
+
+
+    //criando construtor com parâmetros alt+insert
+
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
@@ -27,9 +47,16 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder>{
 
     public class ViewHolder extends RecyclerView.ViewHolder{
 
+        TextView idTituloFilmes;
+        ImageView idImgFilmes;
+        CardView idCardFilmes;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
+            idTituloFilmes = itemView.findViewById(R.id.idTituloFilmes);
+            idImgFilmes = itemView.findViewById(R.id.idImgFilmes);
+            idCardFilmes = itemView.findViewById(R.id.idCardFilmes);
+
         }
     }
 
